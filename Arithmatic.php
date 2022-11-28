cfd8b044d5c919948ff79d1fa2b54542ef84d40a
Patil Amruta <112855985+AmrutaPatil8767@users.noreply.github.com>
2022-11-28 08:10:15 -0600

    Add files via upload

---
+++ Arithmatic.php
@@ -0,0 +1,14 @@
+<?php
+$a=10;
+$b=20;
+$c=$a+$b;
+echo"Addition is $c","<br>";
+$c=$a-$b;
+echo"Subrtaction is $c","<br>";
+$c=$a*$b;
+echo"Multipaction is $c","<br>";
+$c=$a/$b;
+echo"Division is $c","<br>";
+$c=$a%$b;
+echo"Mod is $c","<br>";
+?>
---
+++ Embedding_php_html.php
@@ -0,0 +1,12 @@
+<html>
+<head>
+<title> PHP </title>
+</head>
+<body>
+	<h1>
+	<?php
+         echo"Hi Greeks For Greeks"; 
+         ?>
+	</h1>
+</body>
+</html>
